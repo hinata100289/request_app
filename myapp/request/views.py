1f6121be3c386cc7f1d0b70bebb5ae2247553aff
hinata <yh1502@outlook.jp>
2024-01-23 10:36:41 +0900

    api取得完了

--- myapp/request/views.py
+++ myapp/request/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import requests
+import json
+
+def index(request):
+
+    return render(request, 'request/index.html')
+
+
+def detail(request):
+
+    # indem.htmlから送られてくるデータを取得
+    nyuryoku1 = request.GET['location']
+    # 取得したデータをAPIのURLにくっつける
+    
+    endpoint = "https://weather.tsukumijima.net/api/forecast"+"?city="+nyuryoku1
+    
+    # requests使ってAPIヲたたく
+    response = requests.get(endpoint)
+    print(response.text)
+  
+    # 辞書型に変換
+    dic = response.json()
+    # 必要なデータを取り出す
+    
+
+    context = {
+
+    # 'dic': dic["date"],
+    'dic': dic["forecasts"],
+    }
+    
+
+    return render(request, 'request/detail.html', context)
+
+
